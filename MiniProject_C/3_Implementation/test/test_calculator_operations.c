8ce2fede6b7e75e53f545857825f971ffdbe7c47
Bharathgopal <bharathgopal75@gmail.com>
2020-08-05 12:59:48 +0530

    Added notes

--- MiniProject_C/3_Implementation/test/test_calculator_operations.c
+++ MiniProject_C/3_Implementation/test/test_calculator_operations.c
@@ -1,9 +1,11 @@
 #include <CUnit/Basic.h>
 #include <CUnit/CUnit.h>
 
+/* Modify these two lines according to the project */
 #include <calculator_operations.h>
 #define PROJECT_NAME    "Calculator"
 
+/* Prototypes for all the test functions */
 void test_add(void);
 void test_subtract(void);
 void test_multiply(void);
@@ -11,20 +13,22 @@ void test_divide(void);
 
 /* Start of the application test */
 int main() {
-
+/* Note: Do not edit START*/
   /*Initialize and setup the Test Framework */
   if (CUE_SUCCESS != CU_initialize_registry())
     return CU_get_error();
-
   CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
-
-  /* Add tests to the suite */
+/* Note: Do not edit END */
+  
+  
+  /* Add your test functions in this format for testing*/
   CU_add_test(suite, "add", test_add);
   CU_add_test(suite, "subtract", test_subtract);
   CU_add_test(suite, "multiply", test_multiply);
   CU_add_test(suite, "divide", test_divide);
 
 
+/* Note: Do not edit START*/
   /* Setup Test Framework to output the result to Screen */
   CU_basic_set_mode(CU_BRM_VERBOSE);
   
@@ -33,10 +37,11 @@ int main() {
   
   /* Cleaning the Resources used by Unit test framework */
   CU_cleanup_registry();
-
+/* Note: Do not edit END */
   return 0;
 }
 
+/* Write all the test functions */ 
 void test_add(void) {
   CU_ASSERT(30 == add(10, 20));
   
