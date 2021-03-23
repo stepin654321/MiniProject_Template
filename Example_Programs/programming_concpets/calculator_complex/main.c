767376b7a73c227f8eff207b8c5a0d097249a408
28-shravya <66370715+28-shravya@users.noreply.github.com>
2021-03-23 12:15:41 +0530

    SF Id 258092 Added funtions in complex_calculator.c, complex_calculator.h and main.c

--- Example_Programs/programming_concpets/calculator_complex/main.c
+++ Example_Programs/programming_concpets/calculator_complex/main.c
@@ -2,6 +2,38 @@
 #include "complex_calculator.h"
 int main()
 {
-    //TODO Write all the logic for Calculator app
     printf("Complex calculator ");
+    static complex_t a={0,0};
+    static complex_t b={0,0};
+    static complex_t c={0,0};
+    int choice;
+    while(1)
+    {
+        printf("To add complex numbers press 1.\n");
+        printf("To subtract complex numbers press 2.\n");
+        printf("To multiply complex numbers press 3.\n");
+        printf("To divide complex numbers press 4.\n");
+        printf("To exit press 5.\n");
+        printf("Enter the number.\n");
+        scanf("%d", &choice);
+
+        if(choice == 1){
+            error_t res = complex_sum(&a,&b,&c);
+        }
+        if(choice == 2){
+            error_t res = complex_diff(&a,&b,&c);
+                    }
+        if(choice == 3){
+            error_t res = complex_mul(&a,&b,&c);
+        }
+        if(choice == 4){
+            error_t res = complex_div(&a,&b,&c);
+        }
+        
+        if(choice == 5){
+            break;
+        }
+
+    } 
+    return 0;
 }
--- Example_Programs/programming_concpets/calculator_complex/src/complex_calculator.c
+++ Example_Programs/programming_concpets/calculator_complex/src/complex_calculator.c
@@ -1,5 +1,6 @@
 #include "stdlib.h"
 #include "complex_calculator.h"
+#include<math.h>
 
 error_t complex_sum(complex_t* cnum1, complex_t* cnum2, complex_t* csum)
 {
@@ -10,6 +11,44 @@ error_t complex_sum(complex_t* cnum1, complex_t* cnum2, complex_t* csum)
     csum->real = cnum1->real + cnum2->real;
     csum->imaginary = cnum1->imaginary + cnum2->imaginary;
 
+    return SUCCESS;
+
+}
+
+error_t complex_diff(complex_t* cnum1, complex_t* cnum2, complex_t* cdiff)
+{
+    if(NULL == cnum1 || NULL == cnum2)
+        return ERROR_NULL_PTR;
+    
+    
+    cdiff->real = cnum1->real - cnum2->real;
+    cdiff->imaginary = cnum1->imaginary - cnum2->imaginary;
+
+    return SUCCESS;
+
+}
+
+error_t complex_mul(complex_t* cnum1, complex_t* cnum2, complex_t* cmul)
+{
+    if(NULL == cnum1 || NULL == cnum2)
+        return ERROR_NULL_PTR;
+    
+    
+    cmul->real = (cnum1->real * cnum2->real) - (cnum1->imaginary * cnum2->imaginary);
+    cmul->imaginary = (cnum1->real * cnum2->imaginary) + (cnum1->imaginary * cnum2->real);
+
+    return SUCCESS;
+
+}
+
+error_t complex_div(complex_t* cnum1, complex_t* cnum2, complex_t* cdiv)
+{
+    if(cnum2 == NULL || cnum1 ==NULL){
+        return ERROR_NULL_PTR;
+    }
+
+    cdiv->real = ((cnum1->real * cnum2->real) + (cnum1->imaginary * cnum2->imaginary)) / (pow(cnum2->real,2) + pow(cnum2->imaginary,2));
+    cdiv->imaginary = ((cnum1->imaginary * cnum2->real) - (cnum1->real * cnum2->imaginary)) / (pow(cnum2->real,2) + pow(cnum2->imaginary,2));
     return SUCCESS;
 
 }
