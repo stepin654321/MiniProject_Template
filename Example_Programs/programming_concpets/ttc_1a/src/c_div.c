03497e84b66c5067837af9e59d4b8c601e560839
Arnob Chowdhury <as9025@srmist.edu.in>
2021-03-23 10:00:03 +0530

    dead code resolved

--- Example_Programs/programming_concpets/ttc_1a/src/c_div.c
+++ Example_Programs/programming_concpets/ttc_1a/src/c_div.c
@@ -4,8 +4,9 @@ int div(complex_num s1, complex_num s2){
     struct complex_num res;
 
     if(s2.real == 0 || s2.img == 0 ){
-        return -1;
         printf("You cannot divide by 0");
+        return -1;
+        
     }
 
     int a=(((s1.real)*(s2.real))+((s1.img)*(s2.img)))/(pow(s2.real,2)+pow(s2.img,2));
