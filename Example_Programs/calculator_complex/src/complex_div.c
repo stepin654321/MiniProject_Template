2ce3eb833a3384fb2c7cb2e1d65dba1330822db0
Arnob Chowdhury <as9025@srmist.edu.in>
2021-03-23 11:03:50 +0530

    Refactored Testing source code

--- Example_Programs/calculator_complex/src/complex_div.c
+++ Example_Programs/calculator_complex/src/complex_div.c
@@ -4,7 +4,7 @@
 error_t complex_div(complex_t* s1, complex_t* s2, complex_t* res){
 
     if(s2->real == 0 || s2->imaginary == 0 ){
-        printf("You cannot divide by 0");
+        printf("You cannot divide by 0\n");
         return  ERROR_DIV_BY_ZERO;
     }
 
--- Example_Programs/calculator_complex/test/test_complex_calculator.c
+++ Example_Programs/calculator_complex/test/test_complex_calculator.c
@@ -15,77 +15,41 @@ void tearDown()
 {
 }
 
-void test_zero(void)
-{
-    // Can ommi the below intialization as it is done at the declaration time
+// seperate implementation
+
+void test_sum(void){ // -D
+
+    // test zero
     c1.real = 0;
     c1.imaginary = 0;
     
     c2.real = 0;
     c2.imaginary = 0;
-    
-    // Testing sum
     TEST_ASSERT_EQUAL(SUCCESS, complex_sum(&c1, &c2, &result));
     TEST_ASSERT_EQUAL(0, result.real);
     TEST_ASSERT_EQUAL(0, result.imaginary);
-    // Testing subtraction
-    TEST_ASSERT_EQUAL(SUCCESS, complex_sub(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(0, result.real);
-    TEST_ASSERT_EQUAL(0, result.imaginary);
 
-    // Testing multiplication
-    TEST_ASSERT_EQUAL(SUCCESS, complex_mul(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(0, result.real);
-    TEST_ASSERT_EQUAL(0, result.imaginary);
-    // Testing division
-    TEST_ASSERT_EQUAL(ERROR_DIV_BY_ZERO, complex_div(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(0, result.real);
-    TEST_ASSERT_EQUAL(0, result.imaginary);
-}
-void test_positive(void)
-{
+    // test positive
     c1.real = 25;
     c1.imaginary = 15;
     
     c2.real = 75;
     c2.imaginary = 95;
 
-    c3.real = 1;
-    c3.imaginary = -3;
-    c4.real = 1;
-    c4.imaginary = 2;
-    
-    
     TEST_ASSERT_EQUAL(SUCCESS, complex_sum(&c1, &c2, &result));
     TEST_ASSERT_EQUAL(100, result.real);
     TEST_ASSERT_EQUAL(110, result.imaginary);
 
-    // Test sub
-    TEST_ASSERT_EQUAL(SUCCESS, complex_sub(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(-50, result.real);
-    TEST_ASSERT_EQUAL(-80, result.imaginary);
-    // Test Multiplication
-    TEST_ASSERT_EQUAL(SUCCESS, complex_mul(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(450, result.real);
-    TEST_ASSERT_EQUAL(3500, result.imaginary);
-    // Test division
-    TEST_ASSERT_EQUAL(SUCCESS, complex_div(&c3, &c4, &result));
-    TEST_ASSERT_EQUAL(-1, result.real);
-    TEST_ASSERT_EQUAL(-1, result.imaginary);
-}
-
-void test_negative(void)
-{
+    // test negative
     c1.real = -25;
     c1.imaginary = -15;
     
     c2.real = 75;
     c2.imaginary = 95;
-    
+
     TEST_ASSERT_EQUAL(SUCCESS, complex_sum(&c1, &c2, &result));
     TEST_ASSERT_EQUAL(50, result.real);
     TEST_ASSERT_EQUAL(80, result.imaginary);
-    
 
     c1.real = -25;
     c1.imaginary = -15;
@@ -93,28 +57,104 @@ void test_negative(void)
     c2.real = -75;
     c2.imaginary = -95;
 
+    TEST_ASSERT_EQUAL(SUCCESS, complex_sum(&c1, &c2, &result));
+    TEST_ASSERT_EQUAL(-100, result.real);
+    TEST_ASSERT_EQUAL(-110, result.imaginary);
+}
+
+void test_sub(void){
+    // test zero
+    c1.real = 0;
+    c1.imaginary = 0;
+    
+    c2.real = 0;
+    c2.imaginary = 0;
+    TEST_ASSERT_EQUAL(SUCCESS, complex_sub(&c1, &c2, &result));
+    TEST_ASSERT_EQUAL(0, result.real);
+    TEST_ASSERT_EQUAL(0, result.imaginary);
+
+    // test positive
+    c1.real = 25;
+    c1.imaginary = 15;
+    
+    c2.real = 75;
+    c2.imaginary = 95;
+
+    TEST_ASSERT_EQUAL(SUCCESS, complex_sub(&c1, &c2, &result));
+    TEST_ASSERT_EQUAL(-50, result.real);
+    TEST_ASSERT_EQUAL(-80, result.imaginary);
+
+    // test negative
+
+    c1.real = -25;
+    c1.imaginary = -15;
+    
+    c2.real = -75;
+    c2.imaginary = -95;
+
+    TEST_ASSERT_EQUAL(SUCCESS, complex_sub(&c1, &c2, &result));
+    TEST_ASSERT_EQUAL(50, result.real);
+    TEST_ASSERT_EQUAL(80, result.imaginary);
+
+
+}
+void test_mul(void){
+    // test zero
+    c1.real = 0;
+    c1.imaginary = 0;
+    
+    c2.real = 0;
+    c2.imaginary = 0;
+    TEST_ASSERT_EQUAL(SUCCESS, complex_mul(&c1, &c2, &result));
+    TEST_ASSERT_EQUAL(0, result.real);
+    TEST_ASSERT_EQUAL(0, result.imaginary);
+
+    // test positive
+    c1.real = 25;
+    c1.imaginary = 15;
+    
+    c2.real = 75;
+    c2.imaginary = 95;
+    TEST_ASSERT_EQUAL(SUCCESS, complex_mul(&c1, &c2, &result));
+    TEST_ASSERT_EQUAL(450, result.real);
+    TEST_ASSERT_EQUAL(3500, result.imaginary);
+
+    // test negative
+    c1.real = -25;
+    c1.imaginary = -15;
+    
+    c2.real = -75;
+    c2.imaginary = -95;
+    TEST_ASSERT_EQUAL(SUCCESS, complex_mul(&c1, &c2, &result));
+    TEST_ASSERT_EQUAL(450, result.real);
+    TEST_ASSERT_EQUAL(3500, result.imaginary);
+}
+
+void test_div(void){
+    // test zero
+    c1.real = 0;
+    c1.imaginary = 0;
+    
+    c2.real = 0;
+    c2.imaginary = 0;
+    
+    TEST_ASSERT_EQUAL(ERROR_DIV_BY_ZERO, complex_div(&c1, &c2, &result));
+    // test positive
+    c3.real = 1;
+    c3.imaginary = -3;
+    c4.real = 1;
+    c4.imaginary = 2;
+    TEST_ASSERT_EQUAL(SUCCESS, complex_div(&c3, &c4, &result));
+    TEST_ASSERT_EQUAL(-1, result.real);
+    TEST_ASSERT_EQUAL(-1, result.imaginary);
+    // test negative
     c3.real = -5;
     c3.imaginary = -5;
     c4.real = -5;
     c4.imaginary = 5;
-    
-    TEST_ASSERT_EQUAL(SUCCESS, complex_sum(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(-100, result.real);
-    TEST_ASSERT_EQUAL(-110, result.imaginary);
-
-    // Test sub
-    TEST_ASSERT_EQUAL(SUCCESS, complex_sub(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(50, result.real);
-    TEST_ASSERT_EQUAL(80, result.imaginary);
-    // Test Multiplication
-    TEST_ASSERT_EQUAL(SUCCESS, complex_mul(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(450, result.real);
-    TEST_ASSERT_EQUAL(3500, result.imaginary);
-    // Test division
     TEST_ASSERT_EQUAL(SUCCESS, complex_div(&c3, &c4, &result));
     TEST_ASSERT_EQUAL(0, result.real);
     TEST_ASSERT_EQUAL(1, result.imaginary);
-
 }
 
 int main(void)
@@ -123,9 +163,10 @@ int main(void)
     UNITY_BEGIN();
 
     /* Run Test functions */
-    RUN_TEST(test_zero);
-    RUN_TEST(test_positive);
-    RUN_TEST(test_negative);
+    RUN_TEST(test_sum);
+    RUN_TEST(test_sub);
+    RUN_TEST(test_mul);
+    RUN_TEST(test_div);
 
     /* Close the Unity Test Framework */
     return UNITY_END();
