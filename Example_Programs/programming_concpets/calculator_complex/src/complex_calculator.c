8cd0caaf9448528cf0570d215193c1e97b2c84b5
28-shravya <66370715+28-shravya@users.noreply.github.com>
2021-03-23 13:12:17 +0530

    Modified test cases.
    All the test cases are passed. More test cases are yet to be added.

--- Example_Programs/programming_concpets/calculator_complex/src/complex_calculator.c
+++ Example_Programs/programming_concpets/calculator_complex/src/complex_calculator.c
@@ -43,12 +43,19 @@ error_t complex_mul(complex_t* cnum1, complex_t* cnum2, complex_t* cmul)
 
 error_t complex_div(complex_t* cnum1, complex_t* cnum2, complex_t* cdiv)
 {
-    if(cnum2 == NULL || cnum1 ==NULL){
-        return ERROR_NULL_PTR;
+    if (cnum2->real==0 && cnum2->imaginary==0)
+    {
+        return ERROR_DIV_BY_ZERO;
     }
 
+    else if(cnum2 == NULL || cnum1 ==NULL)
+        return ERROR_NULL_PTR;
+    
+    else{
     cdiv->real = ((cnum1->real * cnum2->real) + (cnum1->imaginary * cnum2->imaginary)) / (pow(cnum2->real,2) + pow(cnum2->imaginary,2));
     cdiv->imaginary = ((cnum1->imaginary * cnum2->real) - (cnum1->real * cnum2->imaginary)) / (pow(cnum2->real,2) + pow(cnum2->imaginary,2));
+    
     return SUCCESS;
+    }
 
 }
