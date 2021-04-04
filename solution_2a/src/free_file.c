689ddcafc857e915c2e187a5c7cc24a88e1a47d5
ayushiag882 <44777159+ayushiag882@users.noreply.github.com>
2021-04-04 14:17:47 +0530

    Delete solution_2a directory

--- solution_2a/src/free_file.c
+++
@@ -1,5 +0,0 @@
-#include "header.h"
-
-void freeMemory(information *user){
-    free(user);
-}
--- solution_2a/src/read_file.c
+++
@@ -1,6 +0,0 @@
-#include "header.h"
-#include<stdio.h>
-
-int fileRead(FILE **fptr, char *buffer){
-    return fscanf(*fptr,"%s",buffer);
-}
