dea741f1ad1d9bd8048a7b93c53a489b75f5ac36
40010753 <bharath.g@ltts.com>
2021-12-30 16:55:34 +0530

    Added Doxygen Steps

--- MiniProject_C/3_Implementation/inc/calculator_operations.h
+++ MiniProject_C/3_Implementation/inc/calculator_operations.h
@@ -1,8 +1,13 @@
-/** 
-* @file calculator_operations.h
-* Calculator application with 4 mathematical operations
-*
-*/
+/**
+ * @file calculator_operations.h
+ * @author Bharath.G ()
+ * @brief Calculator application with 4 mathematical operations
+ * @version 0.1
+ * @date 2021-12-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef __CALCULATOR_OPERATIONS_H__
 #define __CALCULATOR_OPERATIONS_H__
 
